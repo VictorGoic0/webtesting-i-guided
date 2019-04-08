22c5b3e01c99a6ac75113c10f3200ef03ebd7213
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-08 14:01:17 -0400

    Guided demo

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,7 +1,14 @@
 module.exports = {
-  add,
+  add
 };
 
-function add() {
-  return null;
+function add(a, b) {
+  if (add.arguments.length > 2) {
+    return Array.from(add.arguments).reduce((acc, curr) => acc + curr);
+  } else if (a && b) {
+    return a + b;
+  } else if (Array.isArray(a)) {
+    return a.reduce((acc, curr) => acc + curr);
+  }
+  return a;
 }
--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,29 @@
-const { add } = require('./calculator.js');
+const { add } = require("./calculator.js");
 
 // test away!
+
+// test("It should add two numbers", function() {
+//   // Arranged
+//   const expected = 4;
+//   const value1 = 2;
+//   const value2 = 2;
+//   // Act
+//   const sum = add(value1, value2);
+//   // Assert
+//   expect(sum).toEqual(expected);
+// });
+test("It should add two numbers", function() {
+  expect(add(2, 2)).toBe(4);
+  expect(add(3, 3)).toBe(6);
+  expect(add(5, 5)).toBe(10);
+});
+
+test("Should return one value if only one is passed in", function() {
+  expect(add(2)).toBe(2);
+});
+
+test("Should return only number if string and number are passed in", function() {
+  expect(add(2, 2, 2)).toBe(6);
+  expect(add(2, 4, 6, 8)).toBe(20);
+  expect(add([2, 4, 6, 8])).toBe(20);
+});
